3b7c0ec4205d58a3caf4ca94788ca8fac7affd2f
R0driGOOD <Rodrigo_Cabral_2001@hotmail.com>
2022-03-30 06:12:18 -0300

    Atualizando estudos

---
+++ aula11/ex007.js
@@ -0,0 +1,6 @@
+var vel = 44.5
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if(vel >= 70){
+    console.log('Ta com pressa Carai???')
+}else(console.log('é bom que nao corra mermo não.'))
+console.log(`Dirija sempre usando cinto de segurança BAITOLA.`)
